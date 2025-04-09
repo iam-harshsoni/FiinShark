eb7a5b9048d187eafe6088dfc33c113ce3c377de
harsh soni <harshsoni6011@gmail.com>
2025-04-09 13:17:49 +0530

    Add project files.

---
+++ api/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreateOn { get; set; } = DateTime.Now;
+
+        // (1-x) Relation
+        public int? StockId { get; set; }
+
+        [ForeignKey("StockId")]
+        public Stock? Stock { get; set; }
+    }
+}
