5935a356c85160f32bca0104f636a8f56edad88c
harsh soni <harshsoni6011@gmail.com>
2025-04-10 16:44:43 +0530

    refactor: Cleaned the stock Controller by removing all the comments thats are add before.

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -20,17 +20,8 @@ namespace api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            /*
-                Select is a dotnet version of map which is in jS.
-                we are transferring every record to the StockDto from Stock 
-             */
-
-            // Here we are getting all the records with all the fields of Stock Model,
-            // getting it in List() and then mapping it to Stockdto using Select(z=>z.toStockDto)
-            // so we will get only those fields that are available in StockDTO.
-
-            var stocks = await _db.Stocks.ToListAsync();            
-            var stockDto = stocks.Select(s => s.ToStockDto()); 
+            var stocks = await _db.Stocks.ToListAsync();
+            var stockDto = stocks.Select(s => s.ToStockDto());
 
             return Ok(stocks); // 200 Status Code.
         }
@@ -45,27 +36,17 @@ namespace api.Controllers
             if (stock == null) return NotFound();
 
             return Ok(stock.ToStockDto());  // 200 Status Code
-            // whatever record we got in stock, will be transferred into ToStockDTO and we are returning it with OK (200) status code. 
-            // In result, we will get 200 OK response, with the json data containing all the properties of StockDTO.
         }
 
         [HttpPost("createStockDto")]
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
-            // This below code ToStockFromCreateDto() method is 'Extension method' which converts 'CreateDto' data which are received from Body of
-            // 'HTTPPost Creat method 'into 'Stock Model' and store it in 'stockModel Variable'
-
-            var stockModel = stockDto.ToStockFromCreateDto(); 
+            var stockModel = stockDto.ToStockFromCreateDto();
 
             await _db.Stocks.AddAsync(stockModel);
             await _db.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetAll), new {id = stockModel.Id }, stockModel.ToStockDto());  //201 Status code, Mostly used for Post(Create New Record)
-
-            // Explainination of last line -  return CreatedAtAction(nameof(GetAll), stockModel.Id, stockModel.ToStockDto());
-            // 1. Returns HTTP 201 Created status to indicate successful creation of the resource.
-            // 2. Uses nameof(GetAll) to set the Location header pointing to the related endpoint (usually should be GetById).
-            // 3. Sends the created stock data (converted to DTO) in the response body to avoid exposing internal entity details.
+            return CreatedAtAction(nameof(GetAll), new { id = stockModel.Id }, stockModel.ToStockDto()); 
         }
 
         [HttpPut]
@@ -81,10 +62,9 @@ namespace api.Controllers
             stockToUpdate.Purchase = updateDto.Purchase;
             stockToUpdate.LastDiv = updateDto.LastDiv;
             stockToUpdate.MarketCap = updateDto.MarketCap;
-             
+
             await _db.SaveChangesAsync();
 
-            //return CreatedAtAction(nameof(GetById), new { id = id}, stockToUpdate.ToStockDto());
             return Ok(stockToUpdate.ToStockDto());  // 200 Status code
         }
 
@@ -102,7 +82,6 @@ namespace api.Controllers
 
             return NoContent();  //204 Status Code
         }
-
 
         [HttpGet("countStocks")]
         public IActionResult CountStock()
